7f73999c2480e2943e745e358978bdad1565e5bf
adaildoneto <adaildo.neto@gmail.com>
2017-09-16 01:06:08 -0500

    ok

--- index.php
+++ index.php
@@ -30,7 +30,6 @@ get_header(); ?>
 </div>
 
 
-<<<<<<< HEAD
 <div class="col s12 l4">
 		<ul class="collection semmargem" >
 <?php
@@ -63,47 +62,6 @@ wp_reset_query();?>
 
 
 
-=======
-	 <div class="col s12 l3" data-pg-id="142" data-pg-collapsed>
-			 <ul class="collection" data-pg-id="149">
-					 <li class="collection-item avatar" data-pg-id="150">
-							 <img src="images/yuna.jpg" alt="" class="circle" data-pg-id="151">
-							 <span class="title" data-pg-id="152">Title</span>
-							 <p data-pg-id="153">First Line <br data-pg-id="154">
-		Second Line</p>
-							 <a href="#!" class="secondary-content" data-pg-id="155"><i class="material-icons" data-pg-id="156">grade</i></a>
-					 </li>
-					 <li class="collection-item avatar" data-pg-id="157">
-							 <i class="material-icons circle" data-pg-id="158">folder</i>
-							 <span class="title" data-pg-id="159">Title</span>
-							 <p data-pg-id="160">First Line <br data-pg-id="161">
-		Second Line</p>
-							 <a href="#!" class="secondary-content" data-pg-id="162"><i class="material-icons" data-pg-id="163">grade</i></a>
-					 </li>
-					 <li class="collection-item avatar" data-pg-id="164">
-							 <i class="material-icons circle green" data-pg-id="165">insert_chart</i>
-							 <span class="title" data-pg-id="166">Title</span>
-							 <p data-pg-id="167">First Line <br data-pg-id="168">
-		Second Line</p>
-							 <a href="#!" class="secondary-content" data-pg-id="169"><i class="material-icons" data-pg-id="170">grade</i></a>
-					 </li>
-					 <li class="collection-item avatar" data-pg-id="171">
-							 <i class="material-icons circle red" data-pg-id="172">play_arrow</i>
-							 <span class="title" data-pg-id="173">Title</span>
-							 <p data-pg-id="174">First Line <br data-pg-id="175">
-		Second Line</p>
-							 <a href="#!" class="secondary-content" data-pg-id="176"><i class="material-icons" data-pg-id="177">grade</i></a>
-					 </li>
-					 <li class="collection-item avatar" data-pg-id="178">
-							 <i class="material-icons circle red" data-pg-id="179">play_arrow</i>
-							 <span class="title" data-pg-id="180">Title</span>
-							 <p data-pg-id="181">First Line <br data-pg-id="182">
-		Second Line</p>
-							 <a href="#!" class="secondary-content" data-pg-id="183"><i class="material-icons" data-pg-id="184">grade</i></a>
-					 </li>
-			 </ul>
-	 </div>
->>>>>>> 3bbbdb68d57cf5adbc3835b61a8716fde3669897
 </div>
 </div>
 
