49196e390f78c2671f46e9a5c59257d6aa822238
adaildoneto <adaildo.neto@gmail.com>
2017-11-07 03:36:25 -0500

    v

--- content-home5.php
+++ content-home5.php
@@ -1,9 +1,4 @@
 <div class="newsthumb">
-	<?php $categories = get_the_category();
-				 if ( ! empty( $categories ) ) {
-						 echo '<a class="chip blue darken-4 white-text" href="'. esc_url( get_category_link( $categories[0]->term_id ) ) . '">' . esc_html( $categories[0]->name ) . '</a>';
-						 }?>
-
 	 <a href="<?php the_Permalink()?>" title="<?php the_title();?>">
 		<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'thumbnail' ); ?>
 		<img class="left thumbnews" src="<?php echo $image[0]; ?>">
