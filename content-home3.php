8ea19b61d53a657a69ea8c220ace6b48303ac68b
adaildoneto <adaildo.neto@gmail.com>
2017-11-07 03:34:39 -0500

    fv

--- content-home3.php
+++ content-home3.php
@@ -48,9 +48,7 @@
 						</div>
 				</div>
 				<div class="divider detalhe"></div>
-				<div class="nocanto3 small white-text">
-					<?php $my_date = the_date('M j, Y @ G:i', '<em>', '</em>', FALSE); echo $my_date; ?>
-				</div>
+
 				<div class="nocanto4 tamanho-icones">
 
 				</div>
--- content-home5.php
+++ content-home5.php
@@ -1,6 +1,17 @@
 <div class="newsthumb">
+	<?php $categories = get_the_category();
+				 if ( ! empty( $categories ) ) {
+						 echo '<a class="chip blue darken-4 white-text" href="'. esc_url( get_category_link( $categories[0]->term_id ) ) . '">' . esc_html( $categories[0]->name ) . '</a>';
+						 }?>
+
 	 <a href="<?php the_Permalink()?>" title="<?php the_title();?>">
 		<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'thumbnail' ); ?>
 		<img class="left thumbnews" src="<?php echo $image[0]; ?>">
 		<span class="titulopeuqeno"><?php the_title();?></span></a>
+
+		<div class="categoria nocanto2">
+			<span class="data-post"> <?php the_time('d.m.Y');?> </span>
+			<span class="hora-post"> <?php the_time('G:i');?></span>
+
+		</div>
 </div>
