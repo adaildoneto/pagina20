0ada172b98c46b7285ce5aa3b6969980c3e2e33d
adaildoneto <adaildo.neto@gmail.com>
2017-11-07 01:40:57 -0500

    slider up

---
+++ maisnoticias-destaques.php
@@ -0,0 +1,28 @@
+
+
+  <?php
+  $args = array (
+  'pagination'             => true,
+  'posts_per_page'         => '6',
+  'cat'                    => 64,
+  'ignore_sticky_posts'    => true,
+  'order'                  => 'DESC',
+  'orderby'                => 'date',
+);
+
+// The Query
+$query = new WP_Query( $args );
+
+// The Loop
+while( $query->have_posts() ) {
+
+   $query->the_post();
+      echo '<div class=\'noticias-capa\'>';
+    	get_template_part( 'content', 'home5' );
+      echo '</div>';
+
+
+   wp_reset_postdata();
+
+}
+?>
--- slider.php
+++ slider.php
@@ -11,9 +11,20 @@
 <div class="col s12 m5 l4">
 	<div id="slider-destaque">
 
-	<div class="card red" style="height:400px"></div>
-	<div style="height:400px"> <?php include (TEMPLATEPATH.'/maisnoticias3.php'); ?></div>
+	<div class="card">
+		<img src="http://pagina20.net/v2/wp-content/uploads/2017/11/capa217.jpg" class="responsive-img" alt="">
+		</div>
+	<div style="height:600px"> <?php include (TEMPLATEPATH.'/maisnoticias-destaques.php'); ?></div>
 </div>
+
+<div  class="col s12 m7 l8">
+
+	<div class="card red" style="height:180px"><h3>publicidade</h3>
+
+	</div>
+
+</div>
+
 
 <?php
 wp_reset_query();?>
