eed7cd59be77158bd6a43988d1587bdcb2da8c41
adaildoneto <adaildo.neto@gmail.com>
2017-11-17 18:57:28 -0500

    footer ok e bem legalzinho

--- footer.php
+++ footer.php
@@ -20,8 +20,10 @@
           </div>
           <div class="footer-copyright">
             <div class="container">
-          	<p>&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo esc_url( home_url() ); ?>"><?php bloginfo( 'name' ); ?></a> - <?php _e( 'All rights reserved', 'odin' ); ?> | <?php echo sprintf( __( 'Powered by the <a href="%s" rel="nofollow" target="_blank">Odin</a> forces and <a href="%s" rel="nofollow" target="_blank">WordPress</a>.', 'odin' ), 'http://wpod.in/', 'http://wordpress.org/' ); ?></p>
-            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+          	<p>&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo esc_url( home_url() ); ?>"><?php bloginfo( 'name' ); ?></a> - <?php _e( 'All rights reserved', 'odin' ); ?> | <?php echo sprintf( __( 'Empoderado pelas forças de <a href="%s" rel="nofollow" target="_blank">Odin</a> e guiado na sabedoria do <a href="%s" rel="nofollow" target="_blank">WordPress</a>.', 'odin' ), 'http://wpod.in/', 'http://wordpress.org/' ); ?></p>
+            <a class="grey-text text-lighten-4 right" href="http://www.tntnerd.com.br">
+								<img src="http://pagina20.net/v2/wp-content/uploads/2017/11/tntnerd-logo.png" alt="">
+						</a>
             </div>
           </div>
         </footer>
