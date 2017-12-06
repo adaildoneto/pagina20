2c729f1c38ff1be03a7f2f3a049e99e5effd6056
adaildoneto <adaildo.neto@gmail.com>
2017-12-06 14:32:25 -0500

    bugs

--- functions.php
+++ functions.php
@@ -226,8 +226,8 @@ function publicidade300x300_widgets_init() {
 	register_sidebar( array(
 		'name' => 'publicidade300x300',
 		'id' => 'publicidade300x300s',
-		'before_widget' => '<div id="item" class="col s12 m6 l3"><div class="painel-noticias2 card"><div class="bloco-img-noticia2 especiais-img">',
-		'after_widget' => '</div></div></div>',
+		'before_widget' => '<div id="item" class="col s12 m6 l3"><div class="painel-noticias2 card">',
+		'after_widget' => '</div></div>',
 			) );
 }
 add_action( 'widgets_init', 'publicidade300x300_widgets_init' );
--- searchform.php
+++ searchform.php
@@ -1,3 +1,4 @@
+<<<<<<< current
 <?php
 /**
  * The template for displaying Search Form.
@@ -17,3 +18,30 @@
 		</span><!-- /input-group-btn -->
     </div><!-- /input-group -->
 </form><!-- /searchform -->
+=======
+<?php
+/**
+ * The template for displaying Search Form.
+ *
+ * @package Odin
+ * @since 2.2.0
+ */
+?>
+
+<div id="modal1" class="modal">
+  <div class="modal-content">
+    <form method="get" id="searchform" class="form-inline" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
+   	<div class="input-group">
+   		<input type="search" class="form-control" name="s" id="s" value="<?php echo get_search_query(); ?>" placeholder="<?php esc_attr_e( 'Search', 'odin' ); ?>" />
+   	       </div><!-- /input-group -->
+   </form><!-- /searchform -->
+  </div>
+  <div class="modal-footer">
+    <span class="input-group-btn">
+      <button type="submit" class="btn btn-default blue darken-5" value="<?php esc_attr_e( 'Search', 'odin' ); ?>">
+        <i class="glyphicon glyphicon-search"></i>
+      </button>
+    </span><!-- /input-group-btn -->
+  </div>
+</div>
+>>>>>>> before discard
