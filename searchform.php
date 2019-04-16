926cb41fa4da7edb1649e8e7b656ce09bbc880b6
adaildoneto <adaildo.neto@gmail.com>
2019-04-16 10:04:36 -0500

    sa

--- searchform.php
+++ searchform.php
@@ -7,13 +7,14 @@
  */
 ?>
 
-<form method="get" id="searchform" class="form-inline" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
-	<div class="input-group">
-		<input type="search" class="form-control" name="s" id="s" value="<?php echo get_search_query(); ?>" placeholder="<?php esc_attr_e( 'Search', 'odin' ); ?>" />
-		<span class="input-group-btn">
-			<button type="submit" class="btn btn-default" value="<?php esc_attr_e( 'Search', 'odin' ); ?>">
-				<i class="glyphicon glyphicon-search"></i>
-			</button>
-		</span><!-- /input-group-btn -->
-    </div><!-- /input-group -->
-</form><!-- /searchform -->
+<nav class="white">
+	<div class="nav-wrapper">
+		<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+			<div class="input-field white-text">
+			 <input id="search" type="search" placeholder="Pesquisa" value="<?php echo get_search_query(); ?>" name="s"/>
+				<label class="label-icon" for="search"><i class="material-icons">search</i></label>
+				<i class="material-icons">close</i>
+			</div>
+		</form>
+	</div>
+</nav>
