fe79e4c4241e85f21920ef95a01fd39be5abd317
adaildoneto <adaildo.neto@gmail.com>
2019-04-26 11:37:56 -0500

    ds

--- header.php
+++ header.php
@@ -37,8 +37,24 @@
 </head>
 
 <body class="blue-grey lighten-5">
+	<script>
+	  window.fbAsyncInit = function() {
+	    FB.init({
+	      appId      : '629417637481520',
+	      xfbml      : true,
+	      version    : 'v3.2'
+	    });
+	    FB.AppEvents.logPageView();
+	  };
 
-
+	  (function(d, s, id){
+	     var js, fjs = d.getElementsByTagName(s)[0];
+	     if (d.getElementById(id)) {return;}
+	     js = d.createElement(s); js.id = id;
+	     js.src = "https://connect.facebook.net/en_US/sdk.js";
+	     fjs.parentNode.insertBefore(js, fjs);
+	   }(document, 'script', 'facebook-jssdk'));
+	</script>
 	<header>
 		<div class="row blue darken-3">
 
